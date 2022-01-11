4a9f668bb69791f2d1969f13f5d6ee06f2985217
cm <chenmeng_1919@163.com>
2022-01-11 21:08:21 +0800

    项目初始化

---
+++ src/main/java/com/cm/servlet/FreeMarker03.java
@@ -0,0 +1,22 @@
+package com.cm.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * FreeMarker 常用指令
+ */
+@WebServlet("/f03")
+public class FreeMarker03 extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        // 请求转发跳转到指定的模板页面   template/f03.ftl
+        req.getRequestDispatcher("template/f03.ftl").forward(req,resp);
+    }
+}
